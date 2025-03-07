5dc9d8349e75061cc9a7d52a941fd34efe850d35
zakir ullah <zk743087@gmail.com>
2025-03-08 00:21:21 +0500

    navbar done

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import MyNavbar from './components/navbar';
 
 function App() {
-
+  return(
+    <MyNavbar/>
+  )
 }
 
 export default App;
---
+++ src/components/navbar.jsx
@@ -0,0 +1,40 @@
+import logo from '../assests/images/shopon.pk-1741372708474/Shopon_Logo_F-03__1__3n3z-fn.png'
+import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+import Form from 'react-bootstrap/Form';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+
+export default function MyNavbar(){
+        return (
+        <Container>
+          <Navbar expand="lg" className="bg-body-tertiary">
+            <Container fluid>
+              <Navbar.Brand href="#"><img src={logo} width={100} height={40} /></Navbar.Brand>
+              <Navbar.Toggle aria-controls="navbarScroll" />
+              <Navbar.Collapse id="navbarScroll">
+                <Nav
+                  className="me-auto my-2 my-lg-0"
+                  style={{ maxHeight: '100px' }}
+                  navbarScroll
+                >
+                  <Nav.Link href="#action1" className='text-dark '>GIFT CARDS</Nav.Link>
+                  <Nav.Link href="#action2" className='text-dark'>MOBILES & TABLETS</Nav.Link>
+                  <Nav.Link href="#action2" className='text-dark'>FASHION</Nav.Link>
+                  <Nav.Link href="#action2" className='text-dark'>ELECTRONICS</Nav.Link>
+                </Nav>
+                <Form className="d-flex">
+                  <Form.Control
+                    type="search"
+                    placeholder="Search Products"
+                    className="me-2"
+                    aria-label="Search"
+                  />
+                  <Button variant="outline-success">Search</Button>
+                </Form>
+              </Navbar.Collapse>
+            </Container>
+          </Navbar>
+          </Container>
+        );
+      }
